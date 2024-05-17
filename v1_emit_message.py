0868bef88b956732da319b1a67452bafff1384a4
krh5284 <krh5284@gmail.com>
2024-05-17 15:20:47 -0400

    message variable

--- v1_emit_message.py
+++ v1_emit_message.py
@@ -5,7 +5,7 @@
 
 # add imports at the beginning of the file
 import pika
-
+message = 'Hello World!'
 # create a blocking connection to the RabbitMQ server
 conn = pika.BlockingConnection(pika.ConnectionParameters("LOCALHOST"))
 
@@ -16,10 +16,10 @@ ch = conn.channel()
 ch.queue_declare(queue="hello")
 
 # use the channel to publish a message to the queue
-ch.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+ch.basic_publish(exchange="", routing_key="hello", body=message)
 
 # print a message to the console for the user
-print(" [x] Sent 'Hello World!'")
+print(f"Sent '{message}'")
 
 # close the connection to the server
 conn.close()
